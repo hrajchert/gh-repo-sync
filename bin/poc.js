42056297867e6bccd40874ceab9e95d848dd6c7e
Hernan Rajchert <hrajchert@gmail.com>
2018-05-02 23:30:37 -0300

    chore(poc): Added proof of concept js application that does the same as the initial purescript one

---
+++ bin/poc.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+const request = require('request-promise-native')
+const {readFile} = require('fs')
+
+const api = path => `https://api.github.com/${path}`
+const getRepoUrl = (owner, repo) => api(`repos/${owner}/${repo}`)
+
+const userAgent = {'User-Agent': 'gh-repo-sync'}
+const maybeAuth = token => token ? {"Authorization": `Bearer ${token}`} : {}
+
+const getRepo = (owner, repo, token) =>
+    request({
+        url: getRepoUrl(owner, repo),
+        method: 'GET',
+        headers: {
+            ...userAgent,
+            ...maybeAuth(token)
+        }
+    })
+    .then(res => JSON.parse(res))
+
+
+const readJSON = path => readFileP(path, 'UTF-8')
+                            .then(buffer => buffer.toString())
+                            .then(text => JSON.parse(text))
+
+const validateConfig = (config) => {
+    if (!config.organization) throw 'You need to specify an organization in the config'
+    if (!config.repository) throw 'You need to specify a repository in the config'
+    return config;
+}
+
+function main () {
+    readJSON('./config.json')
+        .then(validateConfig)
+        .then(config => getRepo(config.organization, config.repository, config.githubToken))
+        .then(req => console.log(`Yeay = ${req.id}`))
+        .catch(err => console.error('Err: ', '' + err))
+}
+
+main();
+
+// Promised version of readFile.
+function readFileP (path, opts) {
+    return new Promise((resolve, reject) => {
+        const cb = (err, buffer) => err ? reject(err) : resolve(buffer)
+
+        if (opts) {
+            readFile(path, opts, cb)
+        } else {
+            readFile(path, cb)
+        }
+    })
+}
