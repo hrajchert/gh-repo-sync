c69c24c7282836d7aaf5e77da195a9bfada9beca
Hernan Rajchert <hrajchert@gmail.com>
2018-05-02 23:43:03 -0300

    feat(error): improved ForeignError’s message

--- src/Data/Explain.purs
+++ src/Data/Explain.purs
@@ -8,12 +8,15 @@ import Data.Explain (class Explain, explain)
 import Data.Foldable (foldl)
 import Data.Foreign (ForeignError(TypeMismatch, JSONError, ErrorAtProperty, ErrorAtIndex, ForeignError))
 import Data.List.Types (NonEmptyList)
-import Prelude (show, (<>))
+import Prelude (show, (<>), (==))
 import Utils.String (capitalize)
 
 class Explain a where
   explain :: a -> String
 
+
+instance explainString :: Explain String where
+  explain str = str
 
 instance explainNonEmptyList :: Explain a => Explain (NonEmptyList a) where
   explain :: NonEmptyList a -> String
@@ -23,9 +26,29 @@ instance explainNonEmptyList :: Explain a => Explain (NonEmptyList a) where
 
 instance explainForeignError :: Explain ForeignError where
   explain :: ForeignError -> String
-  explain (ForeignError msg) = msg
-  explain (ErrorAtIndex i e) = "at index " <> show i <> ", " <> explain e
-  explain (ErrorAtProperty prop e) = "on property " <> show prop <> ", " <> explain e
-  explain (JSONError s) = "there was a json error: " <> s
-  explain (TypeMismatch exp act) = "i was expecting " <> show exp <> ", but got " <> show act
+  explain e
+    = case e of
+      (ForeignError msg)       -> msg
+      (ErrorAtIndex _ _)        -> "at position " <> showProperty true e <> " " <> showError e
+      (ErrorAtProperty _ _) -> "property " <> show (showProperty true e) <> " " <> showError e
+      (JSONError s)            -> "invalid json: " <> s
+      (TypeMismatch exp act)   -> "i was expecting " <> show exp <> ", but got " <> show act
+    where
+      showProperty :: Boolean -> ForeignError -> String
+      showProperty initial err = case err of
+        (ErrorAtIndex i e') -> "[" <> show i <> "]" <> showProperty false e'
+        (ErrorAtProperty prop e') ->
+                                      let
+                                        prefix = if initial then "" else "."
+                                      in
+                                        prefix <> prop <> showProperty false e'
+        _ -> ""
+
+      showError :: ForeignError -> String
+      showError err = case err of
+        (ErrorAtIndex _ e') -> showError e'
+        (ErrorAtProperty _ e') -> showError e'
+        (TypeMismatch exp "Undefined")   -> "is undefined"
+        (TypeMismatch exp act)   -> "was expected to be " <> show exp <> ", but is " <> show act
+        e' -> explain e'
 
--- test/Main.purs
+++ test/Main.purs
@@ -1,17 +1,20 @@
 module Test.Main where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
+import Test.Data.ExplainSpec (explainSpec)
+import Test.Data.JSON.ParseForeignSpec (parseForeignSpec)
 import Test.Spec (describe)
+import Test.Spec.QuickCheck (QCRunnerEffects)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
-import Test.Spec.QuickCheck (QCRunnerEffects)
 import Test.Utils.StringSpec (stringSpec)
-import Test.Data.JSON.ParseForeignSpec (parseForeignSpec)
-
 main :: Eff (QCRunnerEffects ()) Unit
 main = run [consoleReporter] do
   describe "Utils" do
     stringSpec
   describe "SimpleJSON" do
     parseForeignSpec
+  describe "Explain" do
+    explainSpec
