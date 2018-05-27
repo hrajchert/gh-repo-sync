dce6dfdf7841b379493c0cf1914af7ebb2156fe1
Hernan Rajchert <hrajchert@gmail.com>
2018-05-27 14:01:15 -0300

    refactor(async): moved async helpers to the async module

--- src/Control/Async.purs
+++ src/Control/Async.purs
@@ -1,12 +1,37 @@
 module Control.Async
   ( Async
+  , ifItWorked
+  , withError
   )
    where
 
+import Prelude 
 
 import Control.Monad.Cont.Trans (ContT)
 import Control.Monad.Eff (Eff)
-import Prelude (Unit)
+import Data.Either (Either(..))
+import Data.Bifunctor (lmap)
 
 -- Async eff a = (Either Error a -> Eff eff Unit) -> Eff eff Unit
 type Async eff = ContT Unit (Eff eff)
+
+
+-- Helper functions to work with Async (Either e a)
+
+-- Helps with do notation, runs the async f if the previous result was successful
+-- kind like then from promises
+ifItWorked :: forall a b e eff
+  .  Either e a 
+  -> (a -> Async eff (Either e b)) 
+  -> Async eff (Either e b)
+ifItWorked maybeA f = 
+  case maybeA of
+    Left err -> pure $ Left err
+    Right c  -> f c
+
+
+withError :: forall eff e e' a 
+  .  Async eff (Either e a) 
+  -> (e -> e') 
+  -> Async eff (Either e' a)
+withError a fe = lmap fe <$> a 
--- src/Main.purs
+++ src/Main.purs
@@ -2,13 +2,12 @@ module Main where
 
 import Prelude
 
-import Control.Async (Async)
+import Control.Async (Async, ifItWorked, withError)
 import Control.File as File
 import Control.Github.Api (GetRepoErrors, getRepo)
 import Control.Monad.Cont.Trans (runContT)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.Explain (class Explain, explain)
 import Data.Github.Repository (Repository)
@@ -18,9 +17,6 @@ import Network.HTTP.Affjax (AJAX)
 import Node.Buffer (BUFFER)
 import Node.FS (FS)
 
----------------------------
--- PROGRAM
----------------------------
 newtype Config = Config
   { githubToken  :: Maybe String
   , organization :: String
@@ -29,10 +25,8 @@ newtype Config = Config
 
 derive instance newtypeConfig :: Newtype Config _
 
-
 instance showConfig :: Show Config  where
   show (Config c) = "{ githubToken: " <> (show (c.githubToken)) <> " }"
-
 
 readConfig :: forall eff. String -> Async (fs :: FS, buffer :: BUFFER | eff) (Either File.ReadJsonError Config)
 readConfig = File.readJsonFile
@@ -48,20 +42,6 @@ instance explainProgramErrors :: Explain ProgramErrors where
   explain (ConfigError err) = "There was an error while reading the configuration file: " <> explain err
   explain (GetRepositoryError err) = "Error fetching the repository: " <> explain err
 
-ifItWorked :: forall a b e eff
-  .  Either e a 
-  -> (a -> Async eff (Either e b)) 
-  -> Async eff (Either e b)
-ifItWorked maybeA f = 
-  case maybeA of
-    Left err -> pure $ Left err
-    Right c  -> f c
-
-withError :: forall eff e e' a 
-  .  Async eff (Either e a) 
-  -> (e -> e') 
-  -> Async eff (Either e' a)
-withError a fe = lmap fe <$> a 
 
 program :: forall eff. Async (fs :: FS, buffer :: BUFFER, ajax :: AJAX | eff) (Either ProgramErrors Repository)
 program = do
