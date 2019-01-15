06b3b35bcf1964b67c30fbbef6df500d4a7f362f
Hernan Rajchert <hrajchert@gmail.com>
2019-01-15 16:02:08 -0300

    refactor(rules): Move rules to its own module

---
+++ src/Data/Rules.purs
@@ -0,0 +1,21 @@
+module Data.Rules
+  ( Rules
+  , boolRule
+  , maybeRules
+  , rule
+  ) where
+
+import Data.Maybe (Maybe(..))
+
+type Rules = Array String
+-- TODO: maybe move somewhere else?
+boolRule :: String -> Boolean -> Rules
+boolRule _   false = []
+boolRule str true  = [str]
+
+maybeRules :: forall a. (a -> Rules) -> Maybe a -> Rules
+maybeRules _ (Nothing) = []
+maybeRules f (Just a) = f a
+
+rule :: String -> Rules
+rule str = [str]
--- src/Github/Settings/BranchProtection.purs
+++ src/Github/Settings/BranchProtection.purs
@@ -14,9 +14,9 @@ import Data.Either (Either(..))
 import Data.Explain (class Explain, explain)
 import Data.JSON.ParseForeign (class ParseForeign, parseForeign)
 import Data.Maybe (Maybe(..))
+import Data.Rules (Rules, boolRule, maybeRules, rule)
 import Foreign (F, Foreign)
 import Github.Api.BranchProtection (BranchProtection(..), GetBranchProtectionErrors(..), RequiredPullRequestReviews, RequiredStatusCheck, getBranchProtection)
-
 
 getBranchProtectionSettings
   :: String -- Access token
@@ -119,7 +119,7 @@ instance explainBranchSettings :: Explain BranchProtectionSettings where
   explain BranchNotProtected = "The branch is not protected"
   explain (ProtectedBranch settings) = "The branch is protected in the following way: " <> (explain $ protectedBranchRules settings)
 
-protectedBranchRules :: ProtectedBranchSettings -> Array String
+protectedBranchRules :: ProtectedBranchSettings -> Rules
 protectedBranchRules { pullRequestReview         -- :: Maybe PullRequestReviewSettings
                      , statusChecks              -- :: Maybe StatusChecksSettings
                      , requireSignedCommits      -- :: Boolean
@@ -131,7 +131,7 @@ protectedBranchRules { pullRequestReview         -- :: Maybe PullRequestReviewSe
                       <> boolRule "Commits pushed to this branch must have verified signatures." requireSignedCommits
                       <> boolRule "All configured restrictions are enforced even to the administrators." includeAdministrators
 
-pullRequestReviewsRules :: PullRequestReviewSettings -> Array String
+pullRequestReviewsRules :: PullRequestReviewSettings -> Rules
 pullRequestReviewsRules
   { requiredApprovingReviews -- :: Int -- TODO: see if we can restrict 1 to 6
   , dismissStale             -- :: Boolean
@@ -141,22 +141,10 @@ pullRequestReviewsRules
    <> boolRule "If a new commit is pushed to the branch, previous approvals will be dismissed." dismissStale
    <> boolRule "There must be an approved review in the PR from one of the designated code owner." requireReviewFromOwner
 
-statusChecksRules :: StatusChecksSettings -> Array String
+statusChecksRules :: StatusChecksSettings -> Rules
 statusChecksRules
   { requireUpToDate   -- :: Boolean
   , checks            -- :: Array String
   } = boolRule "The branch needs to be tested with the latest code" requireUpToDate
    <> checks <#> (\check -> "The status code check " <> show check <> " must be valid before merging the PR.")
 
-
--- TODO: maybe move somewhere else?
-boolRule :: String -> Boolean -> Array String
-boolRule _   false = []
-boolRule str true  = [str]
-
-maybeRules :: forall a. (a -> Array String) -> Maybe a -> Array String
-maybeRules _ (Nothing) = []
-maybeRules f (Just a) = f a
-
-rule :: String -> Array String
-rule str = [str]
