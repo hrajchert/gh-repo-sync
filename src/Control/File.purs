39c82c3431f56309eb984bff7af5b4a71002fbb6
Hernan Rajchert <hrajchert@gmail.com>
2019-01-26 23:11:21 -0300

    refactor(config): Group by the errors to provide better context
    # Conflicts:
    #	src/Control/File.purs
    #	src/Main.purs

--- src/Control/File.purs
+++ src/Control/File.purs
@@ -2,11 +2,13 @@ module Control.File
   ( readJsonFile
   , readFile
   , readTextFile
+  , _readFileError
+  , _readFileJsonParseError
   , ReadJsonFileError
   , ReadFileError
-  , ReadFileErrorImpl
+  , ReadFileErrorImpl (..)
   , JsonParseError
-  , JsonParseErrorImpl
+  , JsonParseErrorImpl (..)
   )
 where
 
@@ -14,21 +16,23 @@ import Prelude
 
 import Control.Async (Async, mapExceptT')
 import Control.Monad.Cont.Trans (ContT(..))
-import Data.Bifunctor (lmap)
 import Control.Monad.Except (ExceptT(..), except)
+import Data.Bifunctor (lmap)
+import Data.Either (Either)
 import Data.Explain (class Explain, explain)
 import Data.Either (Either)
 import Data.JSON.ParseForeign (class ParseForeign, readJSON)
 import Data.List.Types (NonEmptyList)
 import Data.Variant (SProxy(..), Variant, inj)
 import Effect.Class (liftEffect)
-import Effect.Exception (Error)
+import Effect.Exception (Error, message)
 import Foreign (ForeignError)
 import Node.Buffer (Buffer, toString)
 import Node.Encoding (Encoding(..))
 import Node.FS.Async as FS
+import Node.Path (FilePath)
+import Type.Data.Boolean (kind Boolean)
 import Type.Row (RowApply)
-
 infixr 0 type RowApply as ⋃
 
 -------------------------------------------------------------------------------
@@ -46,6 +50,7 @@ readTextFile ::
 readTextFile path = (readFile path) >>= toString' where
   toString' :: forall e. Buffer -> Async e String
   toString' buff = liftEffect $ toString UTF8 buff
+
 
 
 -- -- TODO: Thinking of putting this into a ForeignHelper
@@ -82,6 +87,9 @@ readJsonFile path = do
 -- | Thrown when a readFile was unsuccesful
 type ReadFileError ρ = (readFileError ∷ ReadFileErrorImpl | ρ)
 
+_readFileError :: SProxy "readFileError"
+_readFileError = SProxy
+
 newtype ReadFileErrorImpl
   = ReadFileErrorImpl
     { path  :: String
@@ -89,24 +97,29 @@ newtype ReadFileErrorImpl
     }
 
 instance explainReadFileError :: Explain ReadFileErrorImpl where
-  explain (ReadFileErrorImpl {path, error}) = "Could not read file \"" <> path <> "\""
+  explain (ReadFileErrorImpl {path, error}) = "Could not read file \"" <> path <> "\"" <> message error
 
 readFileError :: ∀ ρ. String -> Error -> Variant (ReadFileError ρ)
-readFileError path error = inj (SProxy :: SProxy "readFileError") (ReadFileErrorImpl { path, error })
+readFileError path error = inj _readFileError (ReadFileErrorImpl { path, error })
+
+---------------------------------------
 
 -- | Thrown when parsing a file as JSON
 type JsonParseError ρ = (readFileJsonParseError ∷ JsonParseErrorImpl | ρ)
 
+-- TODO: unify into either jsonParseError or readFileJsonParseError
+_readFileJsonParseError :: SProxy "readFileJsonParseError"
+_readFileJsonParseError = SProxy
+
 newtype JsonParseErrorImpl
   = JsonParseErrorImpl
-    { path  :: String
+    { path  :: FilePath
     , error :: NonEmptyList ForeignError
     }
 
 instance explainJsonParseError :: Explain JsonParseErrorImpl where
   explain (JsonParseErrorImpl {path, error}) = "There was a problem parsing the json file " <> show path <> ":" <> explain error
 
-jsonParseError :: ∀ ρ. String -> NonEmptyList ForeignError -> Variant (JsonParseError ρ)
-jsonParseError path error = inj (SProxy :: SProxy "readFileJsonParseError") (JsonParseErrorImpl { path, error })
-
+jsonParseError :: ∀ ρ. FilePath -> NonEmptyList ForeignError -> Variant (JsonParseError ρ)
+jsonParseError path error = inj _readFileJsonParseError (JsonParseErrorImpl { path, error })
 
--- src/Main.purs
+++ src/Main.purs
@@ -2,22 +2,23 @@ module Main where
 
 import Prelude
 
-import Control.Async (Async, runAsync)
-import Control.File (ReadJsonFileError)
+import Control.Async (Async, runAsync, mapExceptT')
+import Control.File (JsonParseErrorImpl(..), ReadFileErrorImpl(..), ReadJsonFileError, _readFileError, _readFileJsonParseError)
 import Control.File as File
 import Data.Either (Either(..))
-import Data.Explain (explain)
+import Data.Explain (class Explain, explain)
 import Data.JSON.ParseForeign (class ParseForeign)
 import Data.Maybe (Maybe)
 import Data.Newtype (class Newtype)
+import Data.Variant (SProxy(..), Variant, default, inj, onMatch)
 import Effect (Effect)
 import Effect.Console (log)
+import Effect.Exception (message)
 import Github.Api.Api (AccessToken)
 import Github.Api.Repository (Repository, GetRepoError, getRepo)
 import Github.Entities (OrgName, RepoName)
 import Github.Settings.BranchProtection (BranchProtectionSettings)
 import Type.Row (RowApply)
-
 
 -- Used to join error types together (unicode option+22C3)
 infixr 0 type RowApply as ⋃
@@ -37,16 +38,38 @@ derive instance newtypeConfig :: Newtype Config _
 
 derive newtype instance parseForeignConfig :: ParseForeign Config
 
-readConfig :: ∀ e. String -> Async (ReadJsonFileError e) Config
-readConfig = File.readJsonFile
+type ReadConfigError ρ = (readConfigError ∷ ReadConfigErrorImpl ρ | ρ)
 
--- TODO: If I want to have this type of error message where I add context, I should be able to group
--- by ConfigError, etc
--- instance explainProgramErrors :: Explain ProgramErrors where
---   explain :: ProgramErrors -> String
---   explain (ConfigError err) = "There was an error while reading the configuration file: " <> explain err
---   explain (GetRepositoryError err) = "Error fetching the repository: " <> explain err
-type ProgramErrors = (ReadJsonFileError ⋃ GetRepoError ⋃ Ø)
+newtype ReadConfigErrorImpl e
+  = ReadConfigErrorImpl (Variant (ReadJsonFileError e))
+
+instance explainReadConfigError :: Explain (ReadConfigErrorImpl e) where
+  explain (ReadConfigErrorImpl err) = explain' err where
+    explain'
+      = default "Unknown problem"
+      # onMatch
+        { readFileError: \(ReadFileErrorImpl {error})
+            -> "Can't read the config file: " <> message error
+        , readFileJsonParseError: \(JsonParseErrorImpl {path, error})
+            -> "There was a problem parsing the config file '" <> path <>"':"<> explain error
+        }
+
+
+readConfigError :: ∀ ρ. Variant (ReadJsonFileError ρ) -> Variant (ReadConfigError ρ)
+readConfigError error = inj (SProxy :: SProxy "readConfigError") (ReadConfigErrorImpl error)
+
+groupByReadConfigError :: forall e. Variant (ReadJsonFileError ⋃ ReadConfigError e) -> Variant (ReadConfigError e)
+groupByReadConfigError = onMatch
+  { readFileError: readConfigError <<< inj _readFileError
+  , readFileJsonParseError: readConfigError <<< inj _readFileJsonParseError
+  } identity
+
+readConfig :: ∀ e. String -> Async (ReadConfigError e) Config
+readConfig path = File.readJsonFile path `mapExceptT'` groupByReadConfigError
+
+-------------------------------------------------------------------------------
+
+type ProgramErrors = (ReadConfigError ⋃ GetRepoError ⋃ Ø)
 
 program :: Async ProgramErrors Repository
 program = do
